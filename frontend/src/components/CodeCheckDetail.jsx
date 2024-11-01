b69d18c96117d1baa0e09428e5f3b7f0b63bc8e4
mrizkyff <muhamadrizkyff@gmail.com>
2024-11-01 14:16:04 +0700

    update code check rule detail

--- frontend/src/components/CodeCheckDetail.jsx
+++ frontend/src/components/CodeCheckDetail.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getRuleByFeature } from "../services/api";
 export default function CodeCheckDetail({feature}) {
     const [rule, setRule] = useState('');
+    const [updated_at, setUpdated_at] = useState('');
 
     useEffect(() => {
         getRuleById();
@@ -12,6 +13,7 @@ export default function CodeCheckDetail({feature}) {
             const response = await getRuleByFeature(feature);
             console.log(response);
             setRule(response.rule);
+            setUpdated_at(response.updated_at);
         }
         getRule();
     }
@@ -23,14 +25,17 @@ export default function CodeCheckDetail({feature}) {
                 </span>
             </div>
             
-            <div class="p-4 h-full">
+             {/* Scrollable Content */}
+             <div className="p-4 overflow-y-auto h-full scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
                 <div
-                dangerouslySetInnerHTML={{ __html: rule }}
+                    className="prose prose-sm max-w-none"
+                    dangerouslySetInnerHTML={{ __html: rule }}
                 />
             </div>
+
             <div class="mx-3 border-t border-slate-200 pb-3 pt-2 px-1">
                 <span class="text-sm text-slate-600 font-medium">
-                Last updated: 4 hours ago
+                Last updated: {new Date(updated_at).toDateString()}
                 </span>
             </div>
         </div>

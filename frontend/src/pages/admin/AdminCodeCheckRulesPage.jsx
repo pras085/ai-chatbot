401e0f01b6f8444172f86c887536de63119e23a6
mrizkyff <muhamadrizkyff@gmail.com>
2024-11-03 11:12:45 +0700

    update

--- frontend/src/pages/admin/AdminCodeCheckRulesPage.jsx
+++ frontend/src/pages/admin/AdminCodeCheckRulesPage.jsx
@@ -1,14 +1,42 @@
 import Sidebar from "../../components/Sidebar";
+import React, { useState, useEffect } from "react";
+import RulesList from "../../components/RulesList";
+import { fetchAllRules } from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 function AdminCodeCheckRulesPage() {
+    
+    const [rules, setRules] = useState([]);
+    const [isReloading, setIsReloading] = useState(false);
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        // check username
+        if (localStorage.getItem("username") !== "superadmin") {
+            navigate("/")
+        }
+        
+        // wrap fetchRules async to separate function
+        const fetchRules = async () => {
+            const data = await fetchAllRules();
+            console.log(data);
+            setRules(data);
+        }
+
+        fetchRules();
+        setIsReloading(false);
+    }, [isReloading, navigate]);
+    
+
     return (
         <>
             <div className="flex h-[90vh]">
                 <Sidebar />
-
                 {/* Main Content */}
                 <main className="flex-1 p-6">
                     <h1 className="text-2xl mb-4">Admin Code Check Rules</h1>
+                    <RulesList data={rules}/>
                 </main>
             </div>
         </>
--- frontend/src/pages/admin/AdminKnowledgeBasePage.jsx
+++ frontend/src/pages/admin/AdminKnowledgeBasePage.jsx
@@ -1,6 +1,31 @@
 import Sidebar from "../../components/Sidebar";
+import React, { useState, useEffect } from "react";
+import KnowledgeBaseList from "../../components/KnowledgeBaseList";
+import { useNavigate } from "react-router-dom";
+import { fetchAllKnowledges } from "../../services/api";
 
 function AdminKnowledgeBasePage() {
+    const [knowledges, setKnowledgeBase] = useState([]);
+    const [isReloading, setIsReloading] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // check username
+        if (localStorage.getItem("username") !== "superadmin") {
+            navigate("/")
+        }
+        
+        // wrap fetchRules async to separate function
+        const fetchKnowledges = async () => {
+            const data = await fetchAllKnowledges();
+            console.log(data);
+            setKnowledgeBase(data);
+        }
+        
+        fetchKnowledges();
+        setIsReloading(false);
+    }, [isReloading, navigate])
+
     return (
         <>
             <div className="flex h-[90vh]">
@@ -9,6 +34,7 @@ function AdminKnowledgeBasePage() {
                 {/* Main Content */}
                 <main className="flex-1 p-6">
                     <h1 className="text-2xl mb-4">Admin Knowledge Base Page</h1>
+                    <KnowledgeBaseList knowledges={knowledges} setIsReloading={setIsReloading}/>
                 </main>
             </div>
         </>
